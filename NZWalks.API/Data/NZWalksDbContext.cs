0eb5dffb137c232e585450bcf3bedd831f279e76
Alkin Vezhdi <alkinvejdi@gmail.com>
2025-08-12 14:30:22 +0300

    Add DbContext, update the connection string

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="dbContextOptions"></param>
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        /// <summary>
+        /// DbSet Difficulties
+        /// </summary>
+        public DbSet<Difficulty> Difficulties { get; set; }
+        /// <summary>
+        /// DbSet Regions
+        /// </summary>
+        public DbSet<Region> Regions { get; set; }
+        /// <summary>
+        /// DbSet Walks
+        /// </summary>
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
